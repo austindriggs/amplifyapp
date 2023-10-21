dfd31a9adfa8a505d22734d6dec6d85e63993ef0
austindriggs <ard00029@mix.wvu.edu>
2023-10-21 19:12:04 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>WVU ID: 800397100</h2>
+      <p>Hi I am Austin Driggs</p>
+    </div>
+  );
+}
+
+export default App;
